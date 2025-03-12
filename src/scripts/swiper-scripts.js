01723093340741a60cbfdda429996d1466551e56
AR. Arif <arif@vertis.digital>
2025-03-12 15:50:38 +0600

    Completed the explore section slider

--- src/scripts/swiper-scripts.js
+++ src/scripts/swiper-scripts.js
@@ -7,8 +7,8 @@ export default class SwiperSetup {
 
         this.initSpotsSwiper();
         this.initTestimonialSwiper();
-        this.initExternalButtons();
         this.initTrustedSlider();
+        this.initCentreImageSlider();
     }
 
     initSpotsSwiper() {
@@ -50,6 +50,8 @@ export default class SwiperSetup {
             }
         });
 
+        this.initTestimonialExternalButtons();
+
         // Add the slideChange event listener to update button states
         this.testimonialSlider.on('slideChange', () => {
             this.updateNavigationButtons();
@@ -59,7 +61,7 @@ export default class SwiperSetup {
         this.updateNavigationButtons();
     }
 
-    initExternalButtons() {
+    initTestimonialExternalButtons() {
         const prevButton = $('#testimonial-slider .external-prev');
         const nextButton = $('#testimonial-slider .external-next');
 
@@ -117,5 +119,81 @@ export default class SwiperSetup {
                 }
             }
         });
+    }
+
+    initCentreImageSlider() {
+        const totalSlides = $('.swiper--centres .swiper-slide').length;
+
+        this.centreSlider = new Swiper('.swiper--centres', {
+            speed: 2000,
+            spaceBetween: 30,
+            centerInsufficientSlides: true,
+            keyboard: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1
+                },
+                768: {
+                    slidesPerView: Math.min(totalSlides, 2)
+                },
+                1280: {
+                    slidesPerView: Math.min(totalSlides, 3) // Max 3 per view at 1280px
+                }
+            }
+        });
+
+        this.initCentreSlideExternalNav();
+
+        // Add the slideChange event listener to update button states
+        this.centreSlider.on('slideChange', () => {
+            this.updateCentreSlideNavigationButtons();
+        });
+
+        // Initial check to set the button states
+        this.updateCentreSlideNavigationButtons();
+    }
+
+    initCentreSlideExternalNav() {
+        const prevButton = $('#explore-our-centre .external-prev');
+        const nextButton = $('#explore-our-centre .external-next');
+
+        prevButton.on('click', () => {
+            this.centreSlider.slidePrev();
+        });
+
+        nextButton.on('click', () => {
+            this.centreSlider.slideNext();
+        });
+    }
+
+    updateCentreSlideNavigationButtons() {
+        const prevButton = $('#explore-our-centre .external-prev');
+        const nextButton = $('#explore-our-centre .external-next');
+
+        // Check if the first slide is active to disable the previous button
+        if (this.centreSlider.isBeginning) {
+            prevButton.prop('disabled', true); // Disable the "Previous" button if it's the first slide
+            prevButton.addClass('opacity-30 pointer-events-none');
+        } else {
+            prevButton.prop('disabled', false); // Enable the "Previous" button if it's not the first slide
+            prevButton.removeClass('opacity-30 pointer-events-none');
+        }
+
+        // Check if the last slide is active to disable the next button
+        if (this.centreSlider.isEnd) {
+            nextButton.prop('disabled', true); // Disable the "Next" button if it's the last slide
+            nextButton.addClass('opacity-30 pointer-events-none');
+        } else {
+            nextButton.prop('disabled', false); // Enable the "Next" button if it's not the last slide
+            nextButton.removeClass('opacity-30 pointer-events-none');
+        }
     }
 }
